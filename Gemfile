07bd309fa9bf753603ad6452aaf9c7bb089243cb
Mikel Corcuera <mikel@contovista.com>
2017-11-11 12:42:28 +0100

    Remove middleman-deploy dependency

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
-gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
 gem "rake", "~> 10.5.0"
 gem "middleman-dato"
 gem "middleman-livereload"
--- config.rb
+++ config.rb
@@ -5,20 +5,6 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.branch = 'gh-pages' # or master
-
-  committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
-  commit_message = "Deployed using #{committer_app}"
-
-  if ENV["TRAVIS_BUILD_NUMBER"] then
-    commit_message += " (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
-  end
-
-  deploy.commit_message = commit_message
-end
-
 activate :dato, live_reload: true
 
 # Layouts
